5d11f308bff198b278abff256321abac8bf94a90
Nathan Lyle <brickultra@Nathans-Mac-mini.local>
2021-09-17 14:49:51 -0400

    keys and started coding

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,10 @@
 import "./App.css";
+// import { Route } from "react-router-dom";
+// import Navbar from "./components/Navbar";
+// import Play from "./components/Play";
+// import GameInstructions from "./components/GameInstructions";
+// import GameLeaderboards from "./components/GameLeaderboards";
+
 const KEY = process.env.REACT_APP_AIRTABLE_KEY;
 const BASE = process.env.REACT_APP_AIRTABLE_BASE;
 
