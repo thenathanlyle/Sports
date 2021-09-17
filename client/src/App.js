ba2b1dfb7f0fb72a2b0b6f48c334909e8f628454
Nathan Lyle <brickultra@Nathans-Mac-mini.local>
2021-09-17 15:05:20 -0400

    work on App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,15 @@
 import "./App.css";
-// import { Route } from "react-router-dom";
-// import Navbar from "./components/Navbar";
-// import Play from "./components/Play";
-// import GameInstructions from "./components/GameInstructions";
-// import GameLeaderboards from "./components/GameLeaderboards";
-
-const KEY = process.env.REACT_APP_AIRTABLE_KEY;
-const BASE = process.env.REACT_APP_AIRTABLE_BASE;
+import { Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-      {KEY} : {BASE}
+      <Navbar />
+      <Route path="/">
+        <Home />
+      </Route>
     </div>
   );
 }
