399bbcf702c0a6f0c2525eb2225abd2964b6d783
Nathan Lyle <brickultra@Nathans-Mac-mini.local>
2021-09-17 09:21:59 -0400

    inital

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+const KEY = process.env.REACT_APP_AIRTABLE_KEY;
+const BASE = process.env.REACT_APP_AIRTABLE_BASE;
+
+function App() {
+  return (
+    <div className="App">
+      {KEY} : {BASE}
+    </div>
+  );
+}
+
+export default App;
