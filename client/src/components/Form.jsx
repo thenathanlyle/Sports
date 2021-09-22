80404dbbc694c2843e8a9deec572f3d2631d39ad
Nathan Lyle <brickultra@Nathans-Mac-mini.local>
2021-09-22 10:03:56 -0400

    added label to form

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,17 +1,23 @@
 export default function Form(props) {
   return (
     <form onSubmit={props.handleSubmit}>
-      <input
-        type="text"
-        value={props.username}
-        onChange={(e) => props.setUsername(e.target.value)}
-      />
+      <label>
+        Username:
+        <input
+          type="text"
+          value={props.username}
+          onChange={(e) => props.setUsername(e.target.value)}
+        />
+      </label>
       <br />
-      <input
-        type="text"
-        value={props.comment}
-        onChange={(e) => props.setComment(e.target.value)}
-      />
+      <label>
+        Comment:
+        <input
+          type="text"
+          value={props.comment}
+          onChange={(e) => props.setComment(e.target.value)}
+        />
+      </label>
       <br />
       <button>{props.type} Comment</button>
     </form>
