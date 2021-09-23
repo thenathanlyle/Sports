ae13881613c982ff32a6999ff0fbf15a1e378dd6
Nathan Lyle <brickultra@Nathans-Mac-mini.local>
2021-09-23 14:39:10 -0400

    footer fixed

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,12 +1,23 @@
-import { Link } from "react-router-dom";
 export default function Footer() {
   return (
-    <div>
-      <footer>
-        <div className="Foot">
-          <h3>© Nathan Lyle</h3>
-        </div>
-      </footer>
+    <div className="Footer">
+      <div className="Links">
+        <a href="https://github.com/thenathanlyle" target="_blank">
+          <img
+            src="https://res.cloudinary.com/briandanger/image/upload/v1568954107/github_fpykxh.png"
+            alt="GitHub Logo"
+            rel="no referrer"
+          />
+        </a>
+        <a href="https://www.linkedin.com/in/thenathanlyle/" target="_blank">
+          <img
+            src="https://res.cloudinary.com/briandanger/image/upload/v1568954107/linkedin_vnvo6s.png"
+            alt="LinkedIn Logo"
+            rel="no referrer"
+          />
+        </a>
+      </div>
+      © Nathan Lyle
     </div>
   );
 }
